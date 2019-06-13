f81d6952242702e689910e91a29788729365e2b0
Andrew Chubatiuk <achubatiuk@Andrews-MacBook-Pro.local>
2019-06-13 12:33:32 +0300

    fixed elb logs regexp

--- logs/elb.go
+++ logs/elb.go
@@ -10,23 +10,23 @@ type ELBLog struct {
 	ClientPort            string `json:"client_port" regexp:"[0-9]*"`
 	_                     string `regexp:" "`
 	BackendIP             string `json:"backend_ip" regexp:"[^ ]*"`
-	_                     string `regexp:":"`
+	_                     string `regexp:"[:-]"`
 	BackendPort           string `json:"backend_port" regexp:"[0-9]*"`
 	_                     string `regexp:" "`
-	RequestProcessingTime string `json:"request_processing_time" regexp:"[.0-9]*"`
+	RequestProcessingTime string `json:"request_processing_time" regexp:"[-.0-9]*"`
 	_                     string `regexp:" "`
-	BackendProcessingTime string `json:"backend_processing_time" regexp:"[.0-9]*"`
+	BackendProcessingTime string `json:"backend_processing_time" regexp:"[-.0-9]*"`
 	_                     string `regexp:" "`
-	ClientResponseTime    string `json:"client_response_time" regexp:"[.0-9]*"`
+	ClientResponseTime    string `json:"client_response_time" regexp:"[-.0-9]*"`
 	_                     string `regexp:" "`
-	ELBResponseCode       string `json:"elb_response_code" regexp:"-|[0-9]*"`
+	ELBResponseCode       string `json:"elb_response_code" regexp:"-|[-0-9]*"`
 	_                     string `regexp:" "`
-	BackendResponseCode   string `json:"backend_response_code" regexp:"-|[0-9]*"`
+	BackendResponseCode   string `json:"backend_response_code" regexp:"-|[-0-9]*"`
 	_                     string `regexp:" "`
 	ReceivedBytes         string `json:"received_bytes" regexp:"[-0-9]*"`
 	_                     string `regexp:" "`
 	SentBytes             string `json:"sent_bytes" regexp:"[-0-9]*"`
-	_                     string `regexp:" \"`
+	_                     string `regexp:" \""`
 	Method                string `json:"method" regexp:"[^ ]*"`
 	_                     string `regexp:" "`
 	Url                   string `json:"url" regexp:"[^ ]*"`
@@ -34,3 +34,5 @@ type ELBLog struct {
 	Protocol              string `json:"protocol" regexp:"- |[^ ]*"`
 	_                     string `regexp:"\".*"`
 }
+
+([-.0-9]*) ([-.0-9]*) ([-.0-9]*) (|[-0-9]*) (-|[-0-9]*) ([-0-9]*) ([-0-9]*) \\\"([^ ]*) ([^ ]*) (- |[^ ]*)\\\" (\"[^\"]*\") ([A-Z0-9-]+) ([A-Za-z0-9.-]*)$
